d2b4025c19e39fb743ef135c149e2ea4edafb279
Niole Nelson <niolenelson@gmail.com>
2015-05-12 15:56:03 -0400

    list elements appear onMouseOver and stay visible

--- public/js/react_list.js
+++ public/js/react_list.js
@@ -1,15 +1,69 @@
 "use strict";
 /*global React*/
 
-var Test = React.createClass({
+var ListElements = React.createClass({
+  getInitialState: function() {
+    return ( {"list": ["item1","item2","item3"]} );
+  },
   render: function() {
-    return (<h1>Test</h1>);
-    }
+    return( <ReactList listElements={this.state.list}/>);
+  }
+});
+
+var ReactList = React.createClass({
+  propTypes: {
+  listElements: React.PropTypes.array
+  },
+  getInitialState: function() {
+    console.log("getInitialState");
+    var opArr = [];
+    this.props.listElements.forEach( function(e) {
+      opArr.push(0);
+    }.bind(this));
+
+    this.setState( { "opacityArray": opArr } );
+
+    return ({ "opacityArray": opArr });
+  },
+  componentDidMount: function() {
+    console.log("componentDidMount");
+    return ({ "opacityArray": this.state.opacityArray });
+
+  },
+  render: function() {
+    console.log("opacityArray");
+    console.log(this.state.opacityArray);
+
+    return (
+      <div>
+        <h1>I am an Unordered List</h1>
+        <ul>
+          {this.props.listElements.map( function(listElt, i) {
+            console.log('this.stateofopacityarray[i]');
+            console.log(this.state.opacityArray[i]);
+            return( <li style={{"opacity":this.state.opacityArray[i]}} id={i} onMouseOver={this.showElement}>{listElt}</li>);
+            }.bind(this))
+          }
+        </ul>
+      </div>
+    );
+  },
+  showElement: function(event) {
+    event.preventDefault();
+    var targetID = event.target.id;
+    console.log("targetID");
+    console.log(targetID);
+
+    this.state.opacityArray[targetID] = 1;
+    console.log("updated opacityArray");
+    console.log(this.state.opacityArray);
+    this.setState( { "opacityArray": this.state.opacityArray} );
+  }
 });
 
 $(document).ready( function() {
   React.render(
-    <Test/>,
+    <ListElements/>,
     $('#container')[0]
   );
 });
